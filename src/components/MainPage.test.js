f7258a5d8ae28f2189a1ca0e16e6f6c7b339d266
Juan Daniel Vargas <juandanielvargascordero@gmail.com>
2019-08-16 13:37:41 -0600

    Adding unit tests using jest

---
+++ src/components/MainPage.test.js
@@ -0,0 +1,43 @@
+import { shallow, mount, render } from 'enzyme';
+import React from 'react';
+import MainPage from './MainPage';
+
+let wrapper;
+
+beforeEach(() => {
+  const mockProps = {
+    onRequestRobots: jest.fn(),
+    robots: [],
+    searchField: '',
+    isPending: false
+  }
+  wrapper = shallow(<MainPage {...mockProps}/>)
+})
+
+it('renders MainPage without crashing', () => {
+  expect(wrapper).toMatchSnapshot();
+})
+
+it('filters robots correctly', () => {
+  const mockProps2 = {
+    onRequestRobots: jest.fn(),
+    robots: [{
+      id: 1,
+      name: 'John',
+      email: 'john@gmail.com'
+    },
+    {
+      id: 2,
+      name: 'Juan',
+      email: 'juan@gmail.com'
+    }],
+    searchField: 'john',
+    isPending: false
+  }
+  const wrapper2 = shallow(<MainPage {...mockProps2}/>)
+  expect(wrapper2.instance().filterRobots()).toEqual([{
+    id: 1,
+    name: 'John',
+    email: 'john@gmail.com'
+  }])
+})
